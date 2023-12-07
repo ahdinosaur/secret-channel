068b9363e71b937fb5cf8428ebed464d80d23486
Michael Williams <michael.williams@enspiral.com>
2023-12-08 01:21:03 +1300

    test flipped bits

--- js/pull-secret-channel/test/basics.js
+++ js/pull-secret-channel/test/basics.js
@@ -1,11 +1,12 @@
 const test = require('node:test')
 const assert = require('node:assert')
 const pull = require('pull-stream')
+const pullBitflipper = require('pull-bitflipper')
 const { randomBytes } = require('crypto')
 const { pullEncrypter, pullDecrypter, KEY_SIZE, NONCE_SIZE } = require('../')
 const { randomInt } = require('node:crypto')
 
-test.skip('encrypt and decrypt: simple', async (t) => {
+test('encrypt and decrypt: simple', async (t) => {
   // generate a random secret, `KEYBYTES` bytes long.
   const key = randomBytes(KEY_SIZE)
   // generate a random nonce, `NONCE_SIZE` bytes long.
@@ -31,14 +32,11 @@ test.skip('encrypt and decrypt: simple', async (t) => {
 })
 
 test('encrypt and decrypt: random buffers', async (t) => {
-  // generate a random secret, `KEYBYTES` bytes long.
   const key = randomBytes(KEY_SIZE)
-  // generate a random nonce, `NONCE_SIZE` bytes long.
   const nonce = randomBytes(NONCE_SIZE)
-
   const inputBuffers = randomBuffers(randomInt(1e2, 1e3), () => randomInt(1, 1e5))
 
-  return new Promise((resolve, reject) => {
+  await new Promise((resolve, reject) => {
     pull(
       pull.values(inputBuffers),
       pullEncrypter(key, nonce),
@@ -50,6 +48,27 @@ test('encrypt and decrypt: random buffers', async (t) => {
         const output = Buffer.concat(outputBuffers)
         assert.equal(output.length, input.length)
         assert.equal(output.toString('utf8'), input.toString('utf8'))
+        resolve()
+      }),
+    )
+  })
+})
+
+test('detect flipped bits', async (t) => {
+  const key = randomBytes(KEY_SIZE)
+  const nonce = randomBytes(NONCE_SIZE)
+  const inputBuffers = randomBuffers(100, () => 1024)
+
+  await new Promise((resolve, _reject) => {
+    pull(
+      pull.values(inputBuffers),
+      pullEncrypter(key, nonce),
+      pullBitflipper(0.2),
+      pullDecrypter(key, nonce),
+      pull.collect((err, outputBuffers) => {
+        console.error(err)
+        assert.ok(err)
+        assert.notEqual(outputBuffers.length, inputBuffers.length)
         resolve()
       }),
     )
